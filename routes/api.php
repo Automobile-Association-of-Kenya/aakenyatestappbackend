425c50c89be787a6db9d0d990f5861569c309564
abumutai <abrahammutai05@gmail.com>
2021-06-25 00:28:12 +0300

    last changes

--- routes/api.php
+++ routes/api.php
@@ -38,33 +38,33 @@ Route::group(['middleware'=>['auth:api']],function(){
     Route::post('/profile/{id}',[MobileRoutesController::class,'updateprofile']);
 
     //Topics 
-    Route::get('/topics',[MobileRoutesController::class,'topics']);//Get all tests
+    Route::post('/topics',[MobileRoutesController::class,'topics']);//Get all tests
 
     //Tests
-    Route::get('/tests',[MobileRoutesController::class,'tests']);//Get all tests
-    Route::get('/topics/tests',[MobileRoutesController::class,'testspertopic']);//Get tests per topic
-    Route::get('topic/{id}/tests',[MobileRoutesController::class,'testsinatopic']);//Get tests in a specif topic
+    Route::post('/tests',[MobileRoutesController::class,'tests']);//Get all tests
+    Route::post('/topics/tests',[MobileRoutesController::class,'testspertopic']);//Get tests per topic
+    Route::post('topic/{id}/tests',[MobileRoutesController::class,'testsinatopic']);//Get tests in a specif topic
 
     //Grade 
     Route::post('/scores',[MobileRoutesController::class,'scores']);
 
     //Results in a test
-    Route::get('/results',[MobileRoutesController::class,'results']);
+    Route::post('/results',[MobileRoutesController::class,'results']);
 
     //References
-    Route::get('/videos',[MobileRoutesController::class,'videos']);//Videos
-    Route::get('/video/{id}',[MobileRoutesController::class,'video']);//Videos
+    Route::post('/videos',[MobileRoutesController::class,'videos']);//Videos
+    Route::post('/video/{id}',[MobileRoutesController::class,'video']);//Videos
     Route::post('/video/view',[MobileRoutesController::class,'videoviews']);
 
-    Route::get('/pdfs',[MobileRoutesController::class,'pdfs']);
-    Route::get('/pdf/{id}',[MobileRoutesController::class,'pdf']);//PDFS
+    Route::post('/pdfs',[MobileRoutesController::class,'pdfs']);
+    Route::post('/pdf/{id}',[MobileRoutesController::class,'pdf']);//PDFS
     Route::post('/pdf/read',[MobileRoutesController::class,'pdfreads']);
 
     Route::post('/payments',[MobileRoutesController::class,'payments']);
     Route::post('/mypayments',[MobileRoutesController::class,'mypayments']);
-    Route::get('/packages',[MobileRoutesController::class,'packages']);
+    Route::post('/packages',[MobileRoutesController::class,'packages']);
 
-    Route::get('/generatetoken',[MobileRoutesController::class,'generatetoken']);
+    Route::post('/generatetoken',[MobileRoutesController::class,'generatetoken']);
 
 });
 
